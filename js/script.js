67ec5a134497c0296a2b19f68a96de79b892b494
NickusTop <148118087+NickusTop@users.noreply.github.com>
2024-11-18 18:34:05 +0200

    HW

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// 1
+let result = 5 + 5 + '5';
+console.log(result);
+console.log(typeof result);
+// 2
+let email = "nikitashu59@gmail.com";
+console.log(email.includes("@"));
+console.log(email.length);
+// 3 
+let my = "My ";
+let name = "name ";
+let is = "is ";
+let fullName = my + name + is + 'Victor';
+console.log(fullName);
+// 4 
+let userName = "Nickus";
+let payment = 150;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
+
+
+
+
+
+
